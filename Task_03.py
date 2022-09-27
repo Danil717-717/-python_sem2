f485fc36b92ae78755a792e73cf0eef1385298ff
Danil <danila_monster@mail,ru>
2022-09-27 13:52:43 +0300

    Task03

---
+++ Task_03.py
@@ -0,0 +1,10 @@
+#  3. Напишите программу, в которой пользователь будет задавать 
+#  две строки, программа - определять количество вхождений 
+#  одной строки в другой.
+#  in > gipopotampo > po   >> out 3
+#  in > gipopotampo > ta   >> out 1
+
+str_first = input()
+str_second = input()
+
+print(str_first.count(str_second))
