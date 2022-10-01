fafcfeae3b90b0c06ba44c2f5f063715378585a5
Danil <106350420+Danil717-717@users.noreply.github.com>
2022-10-01 17:04:26 +0300

    DZ_005

--- DZ_05.py
+++ DZ_05.py
@@ -4,16 +4,18 @@
 # -> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 # -> [0, 7, 6, 3, 4, 2, 9, 5, 1, 8]
 
+
+from random import randrange
+
 num = int(input())
-num_list = []
+nums_list = list(range(num))
+len_list = len(nums_list)
 
-for i in range(1, num + 1):
-    num_list.append(i - 1)
-print(num_list)
+print(nums_list)
 
-#    не сдюжил!) на семинаре или в комметах 
-#    расскажите решение плиз.
-#    думаю через рекурсию, но не могу понять как)
-#####
+for i in range(len_list):
+    n_1 = randrange(len_list)
+    n_2 = randrange(len_list)
+    nums_list[n_1], nums_list[n_2] = nums_list[n_2], nums_list[n_1]
 
-#####
+print(nums_list)
